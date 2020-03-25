99cb6d8827c5493f4435d8ec96f9d24065d38cfe
parkjungah <asdfg98712@gmail.com>
2020-03-25 15:37:53 +0900

    Code Up 기초 100제, Basic 1  - 출력문

---
+++ Cording/src/basic100/test01.java
@@ -0,0 +1,21 @@
+package basic100;
+
+// 기초1 출력문
+public class test01 {
+
+	public static void main(String[] args) {
+		// System.out.println("Hello"); 
+		//System.out.println("Hello World");
+		//System.out.print("Hello"+"\n"+"World");
+		//System.out.println("'Hello'");
+		//System.out.println("\"Hello World\""); // 쌍따옴표 \"테스트\"
+		//System.out.println("\"!@#$%^&*()\"");
+		System.out.println("\"C:\\Download\\hello.cpp\""); // 백슬래시를 출력하기 위해서는 \ => \\ 두번 쓴다.
+		System.out.println("\u250C"+"\u252c"+"┐"); // 유니코드 특수문자로 
+		System.out.println("├┤┤");
+		System.out.println("└┴┘");
+		System.out.println("Hello, World!");
+		System.out.print("Hello,"+"\n"+"World!\n");
+		System.out.println("\"c:\\test\"");
+	}
+}

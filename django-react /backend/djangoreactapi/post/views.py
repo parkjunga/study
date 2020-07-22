bd3f7291a4e6bc73b66fb845bd05565ab5a16095
박정아 <>
2020-07-22 14:50:58 +0900

    BackEnd 및 FrontEnd 분리하여 간단한 테스트 앱 개발

---
+++ django-react /backend/djangoreactapi/post/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+
+class ListPost(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class DetailPost(generics.RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
